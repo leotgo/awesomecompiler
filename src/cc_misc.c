2026dbf1e91c3fd0be653f0731cd1c78058e16d2
Lucas Schnorr <schnorr@inf.ufrgs.br>
2015-02-18 18:06:14 -0200

    initial commit

---
+++ src/cc_misc.c
@@ -0,0 +1,22 @@
+#include "cc_misc.h"
+
+int getLineNumber (void)
+{
+  //implemente esta função
+  return -1;
+}
+
+void yyerror (char const *mensagem)
+{
+  fprintf (stderr, "%s\n", mensagem); //altere para que apareça a linha
+}
+
+void main_init (int argc, char **argv)
+{
+  //implemente esta função com rotinas de inicialização, se necessário
+}
+
+void main_finalize (void)
+{
+  //implemente esta função com rotinas de inicialização, se necessário
+}
