e734c42e3520b68e702adb74640e493970ae8eeb
leotgo <lrgtagliaro@inf.ufrgs.br>
2015-05-18 22:41:43 -0300

    Structure for code instructions + generate labels function partially implemented

--- src/cc_iloc.c
+++ src/cc_iloc.c
@@ -1,3 +1,13 @@
 #include "cc_iloc.h"
 
+typedef struct instruction
+{
+	char* opcode;		// The opcode for this instruction
 
+	char* tgt_reg;		// The target register for the instruction
+	char* src_reg_1;	// The first source register for instruction. May be empty.
+	char* src_reg_2;	// The second source register for the instruction. May also be empty.
+
+	void* valI;		// Pointer to immediate value for some types of operations
+
+} instruction;
